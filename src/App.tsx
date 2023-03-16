cea2fada1444ad03b2ee2248bb33a31012a851ef
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-03-16 14:09:24 +0100

    TS init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
     </>
   );
 }
-
-export default App;
